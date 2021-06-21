6f1e443fde40c8d9a30af25ca96cef4972860cdd
Alex Younger <alexanderyounger@gmail.com>
2021-06-21 13:19:23 -0400

    remove unneeded code

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,28 +8,6 @@ import aboutImage from '../assets/about_image.png';
 import aboutImagePlaceholder from '../assets/about-image-placeholder.png';
 
 function About() {
-  const sliderSettings = {
-    dots: false,
-    infinite: true,
-    arrows: false,
-    speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 6000,
-    pauseOnHover: true,
-    adaptiveHeight: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
   const services = [
     {
       title: 'Web Development',
